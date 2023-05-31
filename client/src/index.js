26d34e6bcdcfd61676d660fd0dba1a49a74961fc
Kiggles <ridgewwoods@gmail.com>
2023-05-31 12:51:46 -0400

    working on home.js from template, added theme

---
+++ client/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#392203',
+            contrastText: '#efcc99',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+        background: {
+            default: '#85643C',
+            paper: '#f3d0a9',
+        },
+    },
+});
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+export default theme;
