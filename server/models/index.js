081a723bf0e62ece1cebc2a05a22550217d42525
DukeLurkin <durkin.lukas@gmail.com>
2023-05-30 20:49:18 -0400

    created all models Index, User, Characters, Shops, Items

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./user');
+const Characters = require('./characters')
+const Shops = require('./shops')
+const Items = require('./items');
+
+module.exports = { User, Characters, Shops, Items };
