0195dba95f5a298ea318288caca5bc9aaa9ace96
DSCoderman <dsenour6@gmail.com>
2023-05-31 13:54:24 -0400

    fixed typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -18,4 +18,37 @@ type Item{
     itemName: String
     itemPrice: Number
 }
+
+type Shops {
+    _id: ID
+    shopName: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+type Query {
+    users: [user]
+    
+    shop(username: String): [Shops]
+    shop(ShopId: ID!): Shops
+    item(itemId: ID!): Items
+    item(shopName: String): [Items]
+    character(username: String): [Characters]
+    character(characterId: ID!): Characters
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addShop(shopName: String!): Shops
+    addCharacter:(characterId: ID!, characterName: String!): Characters
+    removeShop(shopId: ID!): Shops
+    removeCharacter(characterId: ID!): Characters
+    addItem(itemId: ID!, itemName: String!): Items
+    removeItem(itemId: ID!): Items
+}
 `;
+
+module.exports = typeDefs;
