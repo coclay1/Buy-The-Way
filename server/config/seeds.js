cdf2532ab83d48518e20d35f0851aad9fdae14f3
DukeLurkin <durkin.lukas@gmail.com>
2023-05-31 21:35:53 -0400

    added seeds for items and shops

---
+++ server/config/seeds.js
@@ -0,0 +1,64 @@
+const db = require('./connection');
+const { User, Shops, Items, Characters } = require('../models');
+
+db.once('open', async () => {
+await User.deleteMany();
+  await User.create({
+  userName: 'MrHolt45',
+  email: 'eholt@testmail.com',
+  password: 'password12345'
+})
+  console.log('users seeded');
+
+
+await Items.deleteMany();
+  const items = await Items.insertMany([
+  {itemName: 'Holy Moonlight Sword',
+  itemPrice: 100000},
+  {itemName: 'Chikage',
+  itemPrice: 9000},
+  {itemName: 'Buster Sword',
+  itemPrice: 3500},
+  {itemName: 'Dragon Slayer',
+  itemPrice: 10000},
+  {itemName: 'Berserker Armor',
+  itemPrice: 15000},
+  {itemName: 'Elder Wand',
+  itemPrice: 150000},
+  {itemName: 'Mythril Mail',
+  itemPrice: 999999},
+  {itemName: 'Phoenix Tail Wand',
+  itemPrice: 5000},
+]);
+  console.log('Item Created');
+
+
+
+  await Shops.deleteMany();
+  const shop = await Shops.insertMany([
+    {shopsName: 'Swirlies Sword Shop',
+  items:
+  [
+    items[0]._id, items[1]._id, items[2]._id, items[3]._id,
+  ]
+    },
+    {shopsName: 'Armies Armor Shop',
+  items:
+  [
+    items[4]._id, items[6]._id,
+  ]
+    },
+    {shopsName: 'Wandas Wand Shop',
+  items:
+  [
+    items[5]._id, items[7]._id,
+  ]
+    },
+  ])
+
+  
+
+
+
+  process.exit();
+});
