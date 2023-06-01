7a6c4a5f6f1b500e907ff985dc760a4b7cba10f9
coc lay <coc.lay97@gmail.com>
2023-06-01 14:17:34 -0400

    signup skeleton

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -3,9 +3,19 @@ import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { Link } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-
-export default function SignUp () {
+export default SignUp = () => {
     const [formState, setFormState] = useState({
         username: '',
         email: '',
@@ -32,5 +42,31 @@ export default function SignUp () {
             console.log(err);
         }
     }
-    
+    const defaultTheme = createTheme();
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main">
+                <CssBaseline />
+                <Box>
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}></Avatar>
+                    <Typography component="h1" variant="h5">Sign up</Typography>
+                </Box>
+                <Box>
+                    <Grid>
+                        <Grid>
+
+                        </Grid>
+                        <Button>
+
+                        </Button>
+                        <Grid>
+                            <Link>
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    )
 }
