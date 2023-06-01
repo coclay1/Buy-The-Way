f36bb5d7e151cbc90b19a71bcd217490c61c978a
coc lay <coc.lay97@gmail.com>
2023-06-01 13:42:17 -0400

    add avatar tag in login

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -52,6 +52,7 @@ export default function Login() {
                         flexDirection: 'column',
                         alignItems: 'center',
                     }}>
+                    <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}></Avatar>
                     <Typography component="h1" variant="h5">
                         Sign in
                     </Typography>
