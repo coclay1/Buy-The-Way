9a6b2c558d360663aeb2b82a8ed6eed36f6d9050
Kiggles <ridgewwoods@gmail.com>
2023-06-01 00:48:36 -0400

    added mutations for all but character type defs

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,72 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
-      _id
-      tech1
-      tech2
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
     }
   }
 `;
 
-export const CREATE_VOTE = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
 `;
+
+export const ADD_SHOP = gql`
+  mutation addShop($shopName: String!) {
+    addShop(shopName: $shopName) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const REMOVE_SHOP = gql`
+  mutation removeShop($shopId: ID!) {
+    removeShop(shopId: $shopId) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const ADD_ITEM = gql`
+  mutation addItem($itemId: ID!, $itemName: String!) {
+    addItem(itemId: $itemId, itemName: $itemName) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const REMOVE_ITEM = gql`
+  mutation removeItem($itemId: ID!) {
+    removeItem(itemId: $itemId) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+
+type Mutation {
+  createMatchup(tech1: String!, tech2: String!): Matchup
+  createVote(_id: String!, techNum: Int!): Matchup
+}
