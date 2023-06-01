38804fc5b6019114be3eaf9066fad6b3ba0244dc
Kiggles <ridgewwoods@gmail.com>
2023-06-01 11:45:51 -0400

    working on queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,52 @@
+import { gql } from '@apollo/client';
+
+export const SHOP = gql`
+  query getShop($username: String) {
+    shops(username: $username) {
+      _id
+      shopName
+      item {
+        _id
+        itemName
+        itemPrice
+      }
+    }
+  }
+`;
+
+
+export const QUERY_ALL_ITEMS = gql`
+  {
+    items {
+      _id
+      itemName
+      itemPrice
+    }
+  }
+`;
+
+export const QUERY_SHOPS = gql`
+  {
+    shops {
+      _id
+      shopName
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      username
+      shops {
+        _id
+        shopName
+        items {
+          _id
+          itemName
+          itemPrice
+        }
+      }
+    }
+  }
+`;
