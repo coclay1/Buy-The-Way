50b55fa48373077da5a8e55d4dd7c855ae42a511
coc lay <coc.lay97@gmail.com>
2023-06-02 10:20:14 -0400

    login page debug

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -36,152 +36,67 @@ export default function Login() {
     setFormState({
       email: '',
       password: '',
-    });
+    })
+  };
 
   const defaultTheme = createTheme();
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}></Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleFormSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
+    return (
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+            <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}></Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" onSubmit={handleFormSubmit} noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+            </Box>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
           </Box>
-          <Grid item>
-            <Link href="#" variant="body2">
-              {"Don't have an account? Sign Up"}
-            </Link>
-          </Grid>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  )
+        </Container>
+      </ThemeProvider>
+    )
 }
-
-
-
-// import React, { useState } from 'react';
-// import { useMutation } from '@apollo/client';
-// import { ADD_USER } from '../utils/mutations';
-// import Auth from '../utils/auth';
-// import { Link } from 'react-router-dom';
-
-
-// export default SignUp = () => {
-//   const [formState, setFormState] = useState({
-//       username: '',
-//       email: '',
-//       password: ''
-//   });
-//   const [addUser, { error, data }] = useMutation(ADD_USER);
-//   const handleChange = (e) => {
-//       const { name, value } = e.target;
-//       setFormState({
-//           ...formState,
-//           [name]: value,
-//       });
-//   };
-
-//   const handleFormSubmit = async (e) => {
-//       e.preventDefault();
-
-//       try {
-//           const { data } = await addUser({
-//               variables: { ...formState },
-//           });
-//           Auth.login(data.addUser.token)
-//       } catch (err) {
-//           console.log(err);
-//       }
-//   }
-
-//   return (
-//       <main className='flex-row justify-center mb-4'>
-//           <div className='col-12 col-lg-10'>
-//               <div className='card'>
-//                   <h2 className='card-header'>Sign Up</h2>
-//                   <div className='card-body'>
-//                       {data ? (
-//                           <p>Thank you for signing up!</p>
-//                       ) : (
-//                           <form onSubmit={handleFormSubmit}>
-//                               <input
-//                                   className='form-input'
-//                                   placeholder='username'
-//                                   name='username'
-//                                   type='text'
-//                                   value={formState.name}
-//                                   onChange={handleChange} />
-//                               <input
-//                                   className='form-input'
-//                                   placeholder='email'
-//                                   name='email'
-//                                   type='email'
-//                                   value={formState.email}
-//                                   onChange={handleChange} />
-//                               <input
-//                                   className='form-input'
-//                                   placeholder='******'
-//                                   name='password'
-//                                   type='password'
-//                                   value={formState.password}
-//                                   onChange={handleChange} />
-//                               <button
-//                                   className='btn btn-block btn-primary'
-//                                   type='submit'> Sign Up
-//                               </button>
-//                               <div>
-//                                   <p>
-//                                       Already have an account?
-//                                       <Link to="#">Sign In</Link>
-//                                   </p>
-//                               </div>
-//                           </form>
-//                       )}
-//                   </div>
-//               </div>
-//           </div>
-//       </main>
-//   )
