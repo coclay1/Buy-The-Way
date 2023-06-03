d2d6d89c9b3e2af2f4010a8bd112e7d84cf9924f
Kiggles <ridgewwoods@gmail.com>
2023-06-03 14:05:53 -0400

    logic for singleshop

--- client/src/pages/singleshop.js
+++ client/src/pages/singleshop.js
@@ -2,15 +2,45 @@ import React from 'react';
 
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
+import { useMutation, useState } from '@apollo/client';
 import { ADD_ITEM } from '../../utils/mutations';
 
 const SingleShop = () => {
-    const [shopItems, setShopItems] = useState([])
-
-    const [addItem, { error }] = useMutation(ADD_ITEM)
-
-
-
+    const [formState, setFormState] = useState({
+        itemName: '',
+        itemPrice: '',
+      });
+    
+      // Set up our mutation with an option to handle errors
+      const [addItem, { error }] = useMutation(ADD_ITEM);
+    
+      const handleFormSubmit = async (event) => {
+        event.preventDefault();
+    
+        // On form submit, perform mutation and pass in form data object as arguments
+        // It is important that the object fields are match the defined parameters in `ADD_THOUGHT` mutation
+        try {
+          const { data } = addItem({
+            variables: { itemName: formState.itemName, itemPrice: formState.itemPrice },
+          });
+    
+          window.location.reload();
+        } catch (err) {
+          console.error(err);
+        }
+      };
+    
+      const handleChange = (event) => {
+        const { name, value } = event.target;
+    
+        if (name === 'itemName' && value.length <= 280) {
+          setFormState({ ...formState, [name]: value });
+          setCharacterCount(value.length);
+        } else if (name !== 'itemName') {
+          setFormState({ ...formState, [name]: value });
+        }
+      };
 
 }
+
+export default SingleShop;
