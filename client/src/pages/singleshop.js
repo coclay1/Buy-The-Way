c6fe04188881509799b57fc1ffb5e87a201c5cf9
Kiggles <ridgewwoods@gmail.com>
2023-06-04 12:33:53 -0400

    adding single shop content

--- client/src/pages/singleshop.js
+++ client/src/pages/singleshop.js
@@ -6,39 +6,125 @@ import { useMutation } from '@apollo/client';
 import { ADD_ITEM } from '../utils/mutations';
 
 const SingleShop = () => {
-    const [formState, setFormState] = useState({
-        itemName: '',
-        itemPrice: '',
+  const [formState, setFormState] = useState({
+    itemName: '',
+    itemPrice: '',
+  });
+
+  // Set up our mutation with an option to handle errors
+  const [addItem, { error }] = useMutation(ADD_ITEM);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // On form submit, perform mutation and pass in form data object as arguments
+    // It is important that the object fields are match the defined parameters in `ADD_THOUGHT` mutation
+    try {
+      const { data } = addItem({
+        variables: { itemName: formState.itemName, itemPrice: formState.itemPrice },
       });
-    
-      // Set up our mutation with an option to handle errors
-      const [addItem, { error }] = useMutation(ADD_ITEM);
-    
-      const handleFormSubmit = async (event) => {
-        event.preventDefault();
-    
-        // On form submit, perform mutation and pass in form data object as arguments
-        // It is important that the object fields are match the defined parameters in `ADD_THOUGHT` mutation
-        try {
-          const { data } = addItem({
-            variables: { itemName: formState.itemName, itemPrice: formState.itemPrice },
-          });
-    
-          window.location.reload();
-        } catch (err) {
-          console.error(err);
-        }
-      };
-    
-      const handleChange = (event) => {
-        const { name, value } = event.target;
-    
-        if (name === 'itemName' && value.length <= 280) {
-          setFormState({ ...formState, [name]: value });
-        } else if (name !== 'itemName') {
-          setFormState({ ...formState, [name]: value });
-        }
-      };
+
+      window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'itemName' && value.length <= 280) {
+      setFormState({ ...formState, [name]: value });
+    } else if (name !== 'itemName') {
+      setFormState({ ...formState, [name]: value });
+    }
+  };
+
+  const { loading, data } = useQuery(QUERY_SINGLE_SHOP);
+  const items = data?.items || [];
+
+  function Copyright() {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
+
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+
+          <Typography variant="h6" color="inherit" noWrap>
+            Buy the Way
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Album layout
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              Pick a shop below to see what items are available, or create a new shop!
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Main call to action</Button>
+              <Button variant="outlined">Secondary action</Button>
+            </Stack>
+          </Container>
+        </Box>
+        <ShopList
+          items={items}
+          title={shopName}
+        />
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
 
 }
 
